e61b7f367408c0d837cac8745eab7650cb882040
aaa1013 <118359891+AdrianAlcaldeAlzaga@users.noreply.github.com>
2023-03-22 16:28:58 +0100

    Creación clase ActividadSalidaPuerta #1 #2
    #1 
    #2

--- src/p03/c01/ActividadSalidaPuerta.java
+++ src/p03/c01/ActividadSalidaPuerta.java
@@ -1,8 +1,39 @@
 package src.p03.c01;
 
-public class ActividadSalidaPuerta{
+/**
+ * Clase ActividadEntradaPuerta
+ * 
+ * @author Adrián Alcalde Alzaga
+ *
+ */
+public class ActividadSalidaPuerta implements Runnable{
 
-	//
-	// TODO
-	//
+	private static final int NUMENTRADAS = 20;
+		private String puerta;
+		private IParque parque;
+
+		/**
+		 * Constructor de la clase
+		 * 
+		 * @param puerta
+		 * @param parque
+		 */
+		public ActividadEntradaPuerta(String puerta, IParque parque) {
+			this.puerta = puerta;
+			this.parque = parque;
+		}
+
+		@Override
+		public void run() {
+			for (int i = 0; i < NUMENTRADAS; i ++) {
+				try {
+					parque.entrarAlParque(puerta);
+					TimeUnit.MILLISECONDS.sleep(new Random().nextInt(5)*1000);
+				} catch (InterruptedException e) {
+					Logger.getGlobal().log(Level.INFO, "Entrada interrumpida");
+					Logger.getGlobal().log(Level.INFO, e.toString());
+					return;
+				}
+			}
+		}
 }
