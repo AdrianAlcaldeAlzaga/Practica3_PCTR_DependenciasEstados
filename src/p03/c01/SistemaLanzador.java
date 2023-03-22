cd8746bb30000ce700ef490fbc218275e676f784
aaa1013 <118359891+AdrianAlcaldeAlzaga@users.noreply.github.com>
2023-03-22 16:31:32 +0100

    Update SistemaLanzador #3

--- src/p03/c01/SistemaLanzador.java
+++ src/p03/c01/SistemaLanzador.java
@@ -3,12 +3,13 @@ package src.p03.c01;
 public class SistemaLanzador {
 	public static void main(String[] args) {
 		
+		final int Puertas= 5;
 		IParque parque = new Parque(); // TODO
 		char letra_puerta = 'A';
 		
 		System.out.println("¡Parque abierto!");
 		
-		for (int i = 0; i < Integer.parseInt(args[0]); i++) {
+		for (int i = 0; i < Puertas; i++) {
 			
 			String puerta = ""+((char) (letra_puerta++));
 			
@@ -16,9 +17,9 @@ public class SistemaLanzador {
 			ActividadEntradaPuerta entradas = new ActividadEntradaPuerta(puerta, parque);
 			new Thread (entradas).start();
 			
-			// 
-			// TODO
-			//
+			//Creacion de hilos de salida
+			ActividadSalidaPuerta salidas = new ActividadSalidaPuerta(puerta, parque);
+			new Thread (salidas).start();
 			
 			
 		}
